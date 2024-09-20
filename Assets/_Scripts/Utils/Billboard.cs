31d5adf3e743dde496d8a3c8ec6b9c5b0d405307
Rishav Banerjee <rishavbanerjee1710@gmail.com>
2024-09-20 15:17:21 -0700

    basic setup

---
+++ Assets/_Scripts/Utils/Billboard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    [SerializeField] private bool isEnabled = true;
+    [SerializeField] private Transform cam;
+
+    private void Awake()
+    {
+        if (cam != null) return;
+        if (Camera.main == null) return;
+        cam = Camera.main.transform;
+    }
+
+    private void LateUpdate()
+    {
+        if (!isEnabled) return;
+        if(cam == null) Debug.LogWarning("Camera transform not assigned!!");
+        transform.LookAt(cam);
+        transform.Rotate(0, 180, 0);
+    }
+}
---
+++ Assets/_Scripts/Utils/HotSwapColor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class HotSwapColor : MonoBehaviour
+{
+    [SerializeField] private Color color;
+    [SerializeField] private MeshRenderer mr;
+    
+    private MaterialPropertyBlock mpb;
+    private static readonly int ShaderProp = Shader.PropertyToID("_Color");
+
+    private MaterialPropertyBlock Mpb => mpb ??= new MaterialPropertyBlock();
+
+    private void OnEnable()
+    {
+        if (mr == null) mr = GetComponent<MeshRenderer>();
+        ApplyColor();
+    }
+
+    private void OnValidate()
+    {
+        
+        ApplyColor();
+    }
+
+    public void SetColor(Color color)
+    {
+        this.color = color;
+        ApplyColor();
+    }
+
+    private void ApplyColor()
+    {
+        Mpb.SetColor(ShaderProp, color);
+        if(mr != null){
+            mr.SetPropertyBlock(Mpb);
+        }
+    }
+}
